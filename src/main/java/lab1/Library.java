40235d96e5d448dde6992b0ac01fa684cdaf8db6
20625959 <@20625959>
2020-09-14 14:27:20 +0800

    initializing project

---
+++ src/main/java/lab1/Library.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Library {
+	public static void main(String arg[]) {
+		System.out.println("testing");
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
