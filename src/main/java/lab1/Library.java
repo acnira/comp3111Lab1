28a1fa1a74602fc68eb3124a4230ba36b9430915
user <user@DESKTOP-OFKM0LD>
2020-09-14 14:29:52 +0800

    changed main function

--- src/main/java/lab1/Library.java
+++ src/main/java/lab1/Library.java
@@ -2,7 +2,8 @@ package lab1;
 
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("testing");
+		Library lib=new Library();
+		if(lib.someLibraryMethod())System.out.println("testing");
 	}
 	
     public boolean someLibraryMethod() {
